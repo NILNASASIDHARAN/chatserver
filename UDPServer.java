d450d3db7d39275045f74256dba3a0498df54476
NILNASASIDHARAN <36188687+NILNASASIDHARAN@users.noreply.github.com>
2018-04-03 13:27:12 +0530

    Update UDPServer.java

--- UDPServer.java
+++ UDPServer.java
@@ -5,22 +5,28 @@ class UDPServer
 {
    public static void main(String args[]) throws Exception
       {
-         DatagramSocket serverSocket = new DatagramSocket(9876);
+         DatagramSocket serverSocket = new DatagramSocket(9809);
+         BufferedReader inFromServer =new BufferedReader(new InputStreamReader(System.in));
             byte[] receiveData = new byte[1024];
             byte[] sendData = new byte[1024];
+           
             while(true)
                {
+
                   DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                     //    System.out.println("Length "+ receiveData.length);
                   serverSocket.receive(receivePacket);
                   String sentence = new String( receivePacket.getData());
                   System.out.println("Client: " + sentence);
+                  String msg = inFromServer.readLine();
                   InetAddress IPAddress = receivePacket.getAddress();
                   int port = receivePacket.getPort();
-                  String capitalizedSentence = sentence.toUpperCase();
-                  sendData = capitalizedSentence.getBytes();
+
+                //  String capitalizedSentence = msg.toUpperCase();
+                  sendData = msg.getBytes();
                   DatagramPacket sendPacket =new DatagramPacket(sendData, sendData.length, IPAddress, port);
                   serverSocket.send(sendPacket);
+
                }
       }
 }
